d3068296f32e046be22b28aef554cccefc134ea5
artision <art.2413@hotmail.com>
2019-09-20 16:11:41 +0200

    updated navbar

--- src/App.js
+++ src/App.js
@@ -1,56 +1,66 @@
-import React, { useEffect, useState } from 'react';
-import Recipe from './recipe';
-import './App.css';
-import { async } from 'q';
+import React, { useEffect, useState, Component } from "react";
+import Recipe from "./recipe";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
-
-  const APP_ID = 'aad6d6d8';
-  const APP_KEY = '9a26615561ac8d6bf12ac95217f8ebf9';
+  const APP_ID = "aad6d6d8";
+  const APP_KEY = "9a26615561ac8d6bf12ac95217f8ebf9";
 
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState("");
-  const [query, setQuery] = useState('chicken');
+  const [query, setQuery] = useState("recipe");
 
-  useEffect(() =>{
+  useEffect(() => {
     getRecipes();
   }, [query]);
 
-  const getRecipes = async() => {
-    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
     const data = await response.json();
     setRecipes(data.hits);
-  }
+  };
 
   const updateSearch = e => {
     setSearch(e.target.value);
-  }
+  };
 
   const getSearch = e => {
     e.preventDefault();
     setQuery(search);
     setSearch("");
-  }
+  };
 
-  return(
+  return (
     <div className="App">
       <form onSubmit={getSearch} className="search-form">
-        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
-        <button className="search-button" type="submit">Search</button>
-      </form>
-      <div className="recipes">
-      { recipes.map(recipe => (
-        <Recipe 
-          key={recipe.recipe.label} 
-          title={recipe.recipe.label} 
-          calories={recipe.recipe.calories} 
-          image={recipe.recipe.image} 
-          ingredients={recipe.recipe.ingredients}
+        <input
+          className="search-bar"
+          type="text"
+          value={search}
+          onChange={updateSearch}
+          placeholder="Find recipe:"
         />
-      ))}
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+      <hr />
+      <div className="recipes">
+        {recipes.map(recipe => (
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
       </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/recipe.js
+++ src/recipe.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import style from './recipe.module.css';
+import React from "react";
+import style from "./recipe.module.css";
 
-const Recipe = ({title, calories, image, ingredients}) => {
-    return(
-        <div className={style.recipe}>
-            <h1>{title}</h1>
-            <ol>
-                {ingredients.map(ingredient =>(
-                    <li>{ingredient.text}</li>
-                ))}
-            </ol>
-            <p>{calories}</p>
-            <img className={style.image} src={image} alt="" />
-        </div>
-    );
-}
+const Recipe = ({ title, calories, image, ingredients }) => {
+  return (
+    <div className={style.recipe}>
+      <h3>{title}</h3>
+      <ol>
+        {ingredients.map(ingredient => (
+          <li className={style.List}>{ingredient.text}</li>
+        ))}
+      </ol>
+      <p>Calories: {calories}</p>
+      <img className={style.image} src={image} alt="" />
+    </div>
+  );
+};
 
 export default Recipe;
